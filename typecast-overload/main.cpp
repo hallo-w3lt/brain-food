7d7e4e98d20425d52bd54b54b15f8009348a1229
IT&feel CA <hello@itandfeel.com>
2022-08-19 11:33:52 -0400

    added typecast-overload exercise

---
+++ typecast-overload/main.cpp
@@ -0,0 +1,50 @@
+/*
+* Copyright (c) 2022 - @hallo_w3lt
+* IDE Qt Creator
+* Original code from @0utKast at https://bit.ly/3K8AaFz
+*/
+
+#include <QCoreApplication>
+#include <iostream>
+
+class Cent final
+{
+public:
+    Cent(int cents = 0): m_cents{cents} {}
+
+private:
+    int m_cents {0};
+
+public:
+    operator int() const {return m_cents;}
+
+    void setCents(int cents){ m_cents = cents;}
+};
+
+class Eur final
+{
+public:
+    Eur(int eurs = 0): m_eurs {eurs} {}
+
+private:
+    int m_eurs {0};
+
+public:
+    operator Cent() const {return Cent{m_eurs * 100};};
+};
+
+// implicit conversion from a Eur object
+std::string to_cents(Cent cents)
+{
+    return std::to_string(cents);
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Eur eurs{5};
+    std::cout << "euro cents: " << to_cents(eurs) << std::endl;
+
+    return a.exec();
+}
