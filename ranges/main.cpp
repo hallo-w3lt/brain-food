540bd0aa21c8388239f78600714b4146e856c9b3
Víctor Domínguez <hello@itandfeel.com>
2024-12-09 13:06:51 -0400

    using std::iota

--- ranges/main.cpp
+++ ranges/main.cpp
@@ -4,14 +4,14 @@
 */
 
 #include <iostream>
-#include <vector>
 #include <ranges>
 #include <format>
 
 
 int main() {
 
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    //std::vector<int> numbers = {1, 2, 3, 4, 5};
+    auto numbers = std::views::iota(1, 5);
     auto processed = numbers
                      | std::views::filter([](int n) { return n % 2 == 0; })
                      | std::views::transform([](int n) { return n * 2; });
